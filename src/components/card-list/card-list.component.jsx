fc871e1ee05f24e9030e4a7cf850571746c89d35
sskywalker <hkrai39@gmail.com>
2019-12-08 20:50:02 +0530

    added styles to card component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './card-list.style.css';
 
 export const CardList = props =>{
-    return <div>{props.children}</div> ;
+    return <div className="card-list">{props.children}</div> ;
     
 };
 
