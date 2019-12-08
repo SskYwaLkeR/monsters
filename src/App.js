eaa297775a1aaf5a7970939f2593974efc48fe61
sskywalker <hkrai39@gmail.com>
2019-12-08 21:17:01 +0530

    refactored card list and added card component

--- src/App.js
+++ src/App.js
@@ -17,9 +17,7 @@ class App extends Component{
   render(){
     return(
       <div className="App">
-        <CardList>
-        {this.state.monsters.map((monster,i) => <h1 key={i}>{monster.name}</h1>)}          
-        </CardList>
+        <CardList monsters = {this.state.monsters}/>
       </div>
     )
   }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './card-list.style.css';
+import {Card} from '../card/card.component';
 
 export const CardList = props =>{
-    return <div className="card-list">{props.children}</div> ;
+    return <div className="card-list">
+        {props.monsters.map(monster => {
+            return <Card monster={monster}/>
+        })}
+    </div> ;
     
 };
 
