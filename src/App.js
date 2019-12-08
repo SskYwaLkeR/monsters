b4141bae3517308c2e1c54d3d85bcb4cd872dd49
sskywalker <hkrai39@gmail.com>
2019-12-08 20:41:08 +0530

    initial setup plus added card component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {CardList} from './components/card-list/card-list.component.jsx';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      monsters : []
+    };
+  };
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(result => this.setState({monsters: result}))
+  }
+  render(){
+    return(
+      <div className="App">
+        <CardList>
+        {this.state.monsters.map((monster,i) => <h1 key={i}>{monster.name}</h1>)}          
+        </CardList>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './card-list.style.css';
+
+export const CardList = props =>{
+    return <div>{props.children}</div> ;
+    
+};
+
+
