7f5373db2d532871352ed3e61570de7fa4715101
sskywalker <hkrai39@gmail.com>
2019-12-09 21:24:27 +0530

    stylzied home page

--- src/App.js
+++ src/App.js
@@ -28,6 +28,8 @@ class App extends Component{
     return(
       <div className="App">
       
+        <h1>Monsters</h1>
+
         <SearchBox
         placeholder="Search monster"
         handleChange = {this.handleSearch}
